49f0e88dabb057be110c3a2caeabc95b95e26152
Parviz Isaev <parvizjan@inbox.ru>
2023-03-17 02:57:05 +0500

    portfolio push one

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+// SHOW MENU 
+
+// const navToggle = document.querySelector('.nav_toggle'),
+//       navMenu =  document.querySelector('.nav_menu');
+
+//     navToggle.addEventListener('click', ()=> {
+//         navMenu.classList.toggle('show-menu');
+//         navToggle.classList.toggle('active');
+//     });
+
+// TYPING TEXT
+
+// var typed = new Typed(".profession_text",{
+//     strings:["I'M A DATA ANALYST FROM Dushanbe, Tajikistan", "I'M A DATA DATA SCIENTIST FROM Dushanbe, Tajikistan"],
+//     typeSpeed: 70,
+//     backSpeed:70,
+//     loop:true,
+//     loopCount: Infinity,
+// });
+
+// COUNTER
+
+// SWIPER
+
+// BACK TOP BUTTON
+
+// SCROLL REVEAL ANIMATION
