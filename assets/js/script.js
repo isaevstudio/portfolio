da068967f675528f52b88f1e2ffd5c92722e9155
Parviz Isaev <parvizjan@inbox.ru>
2023-03-17 23:44:46 +0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,30 +14,225 @@ AOS.init({
 
 });
 
-// SHOW MENU 
 
-// const navToggle = document.querySelector('.nav_toggle'),
-//       navMenu =  document.querySelector('.nav_menu');
+// CHARTJS
+// Scatter
+var xyValues = [
+  {x:50, y:7},
+  {x:60, y:8},
+  {x:70, y:8},
+  {x:80, y:9},
+  {x:90, y:9},
+  {x:100, y:9},
+  {x:110, y:10},
+  {x:120, y:11},
+  {x:130, y:14},
+  {x:140, y:14},
+  {x:150, y:15}
+];
 
-//     navToggle.addEventListener('click', ()=> {
-//         navMenu.classList.toggle('show-menu');
-//         navToggle.classList.toggle('active');
-//     });
+new Chart("myChart1", {
+  type: "scatter",
+  data: {
+    datasets: [{
+      pointRadius: 4,
+      pointBackgroundColor: '#022a30',
+      data: xyValues
+    }]
+  },
+  options:{title: {
+    display: true,
+    text: "House Prices vs. Size",
+  },
+  legend:{
+    display:false,
+  }
+  }
+});
 
-// TYPING TEXT
 
-// var typed = new Typed(".profession_text",{
-//     strings:["I'M A DATA ANALYST FROM Dushanbe, Tajikistan", "I'M A DATA DATA SCIENTIST FROM Dushanbe, Tajikistan"],
-//     typeSpeed: 70,
-//     backSpeed:70,
-//     loop:true,
-//     loopCount: Infinity,
-// });
 
-// COUNTER
+// DOUGHNUT
+var xValues = ["Smart phone", "Computer", "Tablet", "TV"];
+var yValues = [145, 96, 32, 5];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+function prct(values) {
+  let total = values.reduce((a, b) => a + b, 0);
+  let prctage = values.map(num => (num/total)*100).map(num => num.toFixed(2))
+  return prctage;
+};
 
-// SWIPER
+new Chart("myChart2", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: prct(yValues)
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Devices used to watch live streaming in %"
+    },
+    plugins: {
+      datalabels: {
+          formatter: (value, ctx) => {
 
-// BACK TOP BUTTON
+              let datasets = ctx.chart.data.datasets;
 
-// SCROLL REVEAL ANIMATION
+              if (datasets.indexOf(ctx.dataset) === datasets.length - 1) {
+                  let sum = 0;
+                  datasets.map(dataset => {
+                      sum += dataset.data[ctx.dataIndex];
+                  });
+                  let percentage = Math.round((value / sum) * 100) + '%';
+                  return percentage;
+              } else {
+                  return percentage;
+              }
+          },
+          color: 'black',
+               }
+  }
+  }
+});
+
+
+// BAR CHART
+new Chart(document.getElementById("myChart3"), {
+  type: 'bar',
+  data: {
+    labels: ["2020", "2021", "2022"],
+    datasets: [
+      {
+        label: "Attraction",
+        backgroundColor: "#3e95cd",
+        data: [500,613,783]
+      }, {
+        label: "Churn",
+        backgroundColor: "#8e5ea2",
+        data: [65,87]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Customer Attraction & Churn by year'
+    }
+  }
+});
+
+// MIXED PLOT
+new Chart(document.getElementById("myChart4"), {
+  type: 'bar',
+  data: {
+    labels: ["1900", "1950", "1999", "2050"],
+    datasets: [{
+        label: "Europe",
+        type: "line",
+        borderColor: "#8e5ea2",
+        data: [408,547,675,734],
+        fill: false
+      }, {
+        label: "Africa",
+        type: "line",
+        borderColor: "#3e95cd",
+        data: [133,221,783,2478],
+        fill: false
+      }, {
+        label: "Europe",
+        type: "bar",
+        backgroundColor: "rgba(0,0,0,0.2)",
+        data: [408,547,675,734],
+      }, {
+        label: "Africa",
+        type: "bar",
+        backgroundColor: "rgba(0,0,0,0.2)",
+        backgroundColorHover: "#3e95cd",
+        data: [133,221,783,2478]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Population growth (millions): Europe & Africa'
+    },
+    legend: { display: true }
+  }
+});
+
+// BUBBLE CHART
+new Chart(document.getElementById("myChart5"), {
+  type: 'bubble',
+  data: {
+    labels: "Africa",
+    datasets: [
+      {
+        label: ["China"],
+        backgroundColor: "rgba(255,221,50,0.2)",
+        borderColor: "rgba(255,221,50,1)",
+        data: [{
+          x: 21269017,
+          y: 5.245,
+          r: 15
+        }]
+      }, {
+        label: ["Denmark"],
+        backgroundColor: "rgba(60,186,159,0.2)",
+        borderColor: "rgba(60,186,159,1)",
+        data: [{
+          x: 258702,
+          y: 7.526,
+          r: 10
+        }]
+      }, {
+        label: ["Germany"],
+        backgroundColor: "rgba(0,0,0,0.2)",
+        borderColor: "#000",
+        data: [{
+          x: 3979083,
+          y: 6.994,
+          r: 15
+        }]
+      }, {
+        label: ["Japan"],
+        backgroundColor: "rgba(193,46,12,0.2)",
+        borderColor: "rgba(193,46,12,1)",
+        data: [{
+          x: 4931877,
+          y: 5.921,
+          r: 15
+        }]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050'
+    }, scales: {
+      yAxes: [{ 
+        scaleLabel: {
+          display: true,
+          labelString: "Happiness"
+        }
+      }],
+      xAxes: [{ 
+        scaleLabel: {
+          display: true,
+          labelString: "GDP (PPP)"
+        }
+      }]
+    }
+  }
+});
